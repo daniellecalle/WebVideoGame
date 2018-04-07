a3f81640ee7ac44a7a4cd375029410c44ca9ff18
Daniellecaalle <daniellecalle@hotmail.com>
2018-04-07 15:11:40 -0500

    Subiendo Archivos y directorios del protecto

---
+++ WebVideoGame/WebVideoGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebVideoGame.MvcApplication" Language="C#" %>
---
+++ WebVideoGame/WebVideoGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebVideoGame.Startup))]
+namespace WebVideoGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
